c825e3f078f430b47fd0bda9e26fcffe12b314f3
Gustavo <gustavosohne38@gmail.com>
2020-04-21 00:57:23 -0300

    feito

--- backend/src/controllers/ong-controller.js
+++ backend/src/controllers/ong-controller.js
@@ -6,7 +6,7 @@ module.exports = {
     const { name, email, whatsapp, city, uf } = req.body
     const id = crypto.randomBytes(4).toString('HEX')
 
-    await connection('ongs').insert({
+    const data = await connection('ongs').insert({
       id,
       name,
       email,
@@ -14,11 +14,23 @@ module.exports = {
       city,
       uf,
     })
-    return res.json({ id })
+
+    return res.json({ data })
   },
 
   async get(req, res) {
     const ongs = await connection('ongs').select('*')
     return res.json(ongs)
+  },
+
+  async delete(req, res) {
+    const id = req.params.id
+
+    const deletar = await connection('ongs').where('id', id).delete()
+
+    if (deletar) {
+      return res.status(301).send({ message: 'ONG excluída com sucesso!' })
+    }
+    return res.status(401).send({ message: 'ERRO! ONG não excluída!' })
   },
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,11 +2,19 @@ const express = require('express')
 const routes = express.Router()
 const ongController = require('../src/controllers/ong-controller')
 const incidentsController = require('../src/controllers/incident-controller')
+const profileController = require('../src/controllers/profile-controller')
+const sessionController = require('../src/controllers/session-controller')
 
 routes.post('/ongs', ongController.create)
 routes.get('/ongs', ongController.get)
+routes.delete('/ongs/:id', ongController.delete)
+
 routes.post('/incidents', incidentsController.create)
 routes.get('/incidents', incidentsController.get)
-routes.delete('/incidents/:id', incidentsController.delete)
+routes.delete('/incidents', incidentsController.delete)
+
+routes.get('/profiles', profileController.index)
+
+routes.post('/session', sessionController.post)
 
 module.exports = routes
