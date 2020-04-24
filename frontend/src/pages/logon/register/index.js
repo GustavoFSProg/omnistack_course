5ed99f2ef75414e64db13124e87aed214c50fa83
Gustavo <gustavosohne38@gmail.com>
2020-04-24 16:29:55 -0300

    Last-24-04

--- frontend/src/pages/logon/register/index.js
+++ frontend/src/pages/logon/register/index.js
@@ -14,10 +14,24 @@ export default function Register() {
           <h1>Cadastro</h1>
           <p>Faça seu Cadastro e se comunique conosco e dados da sua ONG</p>
 
-          <Link className="back-link" to="/register">
+          <Link className="back-link" to="/">
             <FiArrowLeft size={16} /> Não tenho cadastro
           </Link>
         </section>
+
+        <form>
+          <input type="text" placeholder="Nome da ONG"></input>
+          <input type="email" placeholder="E-mail"></input>
+          <input placeholder="Whatsapp"></input>
+          <div className="input-group">
+            <input type="text" placeholder="Cidade"></input>
+            <input type="text" placeholder="UF" width="80"></input>
+          </div>
+
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
+        </form>
       </div>
     </div>
   )
